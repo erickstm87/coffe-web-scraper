95c6002c4321d8658cfd9f42a9d922838c985cb2
thomas erickson <terickson@spotxchange.com>
2017-07-15 17:45:19 -0600

    first commit on a new coffee web scraper

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const request = require('request');
+var app = express();
+
+app.get('/scrape', () => {
+
+})
+
+app.listen('8081');
+
+console.log('here\'s the magic on port 8081');
+
+exports = module.exports = app;
